0b852febd85a40fd2377b899dabde6b878c30243
dnnxl15 <dnnxl15@hotmail.com>
2018-10-28 12:38:27 -0600

    Added Person class

---
+++ Aprende_Movil/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Aprende_Movil.Models
+{
+	public class Person
+	{
+		private String name { get; set; }
+		private String lastname { get; set; }
+		private String address { get; set; }
+		private String identification { get; set; }
+		private DateTime dateOfBirth { get; set; }
+	}
+}
