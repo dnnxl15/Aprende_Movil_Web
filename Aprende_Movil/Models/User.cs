4662618cdbf3f2f39459e95b804466e980a0f2bc
dnnxl15 <dnnxl15@hotmail.com>
2018-10-28 12:42:36 -0600

    Added User class

---
+++ Aprende_Movil/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Aprende_Movil.Models
+{
+	public class User
+	{
+		private String email { get; set; }
+		private String password { get; set; }
+	}
+}
